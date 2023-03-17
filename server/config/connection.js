6340a8ae2e17e0f7054c892e0154cef55bd8cde2
erik814 <erikneafus@gmail.com>
2023-03-17 18:26:02 -0500

    server script change.

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,9 +4,6 @@ require('dotenv').config()
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  
-  useFindAndModify: false,
-  useCreateIndex: true,
 });
 
 module.exports = mongoose.connection;
