d447f44f2a55df7f94bbe65d74a221c4ef8d6dda
erik814 <erikneafus@gmail.com>
2023-03-17 16:29:55 -0500

    Server.js change

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(routes);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -13,8 +15,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
